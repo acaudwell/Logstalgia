ff8c3166025d8255d83341db93266e80e06c60fa
Andrew Caudwell <acaudwell@gmail.com>
2013-08-26 19:02:05 +1200

    NSIS installer for Logstalgia.

--- dev/bin/build_win32.pl
+++ dev/bin/build_win32.pl
@@ -1,73 +1,270 @@
 #!/usr/bin/perl
+# windows archive + installer generator
 
 use strict;
 use warnings;
 use FindBin;
 
-my $APP_NAME = 'logstalgia';
+my $base_dir   = "$FindBin::Bin/../..";
+my $dll_dir    = "$base_dir/dev/win32";
+my $builds_dir = "$base_dir/dev/builds";
 
-sub app_version {
-    my $version = `cat $FindBin::Bin/../../src/logstalgia.h | grep LOGSTALGIA_VERSION`;
+sub logstalgia_version {
+    my $version = `cat $base_dir/src/settings.h | grep LOGSTALGIA_VERSION`;
     $version =~ /"([^"]+)"/ or die("could not determine version\n");
     $version = $1;
     return $version;
 }
 
-my $WINBUILD = "$FindBin::Bin/../builds/win32/";
-
-my $VERSION  = app_version();
-
 sub doit {
     my $cmd = shift;
-
+    
     if(system($cmd) != 0) {
-        die("command '$cmd' failed: $!");
+    die("command '$cmd' failed: $!");
     }
 }
 
 sub dosify {
     my($src, $dest) = @_;
-
+    
     my $content = `cat $src`;
     $content =~ s/\r?\n/\r\n/g;
-
+    
     open  OUTPUT, ">$dest" or die("$!");
     print OUTPUT $content;
     close OUTPUT;
 }
 
-chdir("$FindBin::Bin/../../");
+chdir("$base_dir") or die("chdir to $base_dir failed");
 
-doit("rm -rf $WINBUILD");
-doit("mkdir -p $WINBUILD");
-doit("mkdir -p $WINBUILD/data/");
-doit("mkdir -p $WINBUILD/data/fonts/");
+my $nsis_script = q[
+!define MULTIUSER_MUI
+!define MULTIUSER_EXECUTIONLEVEL Highest
+!define MULTIUSER_INSTALLMODE_COMMANDLINE
+!define MULTIUSER_INSTALLMODE_DEFAULT_REGISTRY_KEY "Software\Logstalgia"
+!define MULTIUSER_INSTALLMODE_INSTDIR_REGISTRY_KEY "Software\Logstalgia"
+!define MULTIUSER_INSTALLMODE_DEFAULT_REGISTRY_VALUENAME "Install_Mode"
+!define MULTIUSER_INSTALLMODE_INSTDIR_REGISTRY_VALUENAME "Install_Dir"
+!define MULTIUSER_INSTALLMODE_INSTDIR "Logstalgia"
+!include "MultiUser.nsh"
 
-doit("cp $APP_NAME.exe $WINBUILD");
-doit("cp data/*.tga $WINBUILD/data/");
-doit("cp data/example.log $WINBUILD/data/");
-doit("cp data/fonts/*.ttf $WINBUILD/data/fonts/");
+!include "MUI2.nsh"
+!include "LogicLib.nsh"
+!include "SafeEnvVarUpdate.nsh"
 
-dosify('README',    "$WINBUILD/README.txt");
-dosify('ChangeLog',    "$WINBUILD/ChangeLog.txt");
-dosify('data/fonts/README', "$WINBUILD/data/fonts/README.txt");
-dosify('README-SDL',"$WINBUILD/README-SDL.txt");
-dosify('COPYING',   "$WINBUILD/COPYING.txt");
-dosify('THANKS',    "$WINBUILD/THANKS.txt");
+Name "Logstalgia LOGSTALGIA_VERSION"
 
-doit("cp dev/win32/SDL.dll $WINBUILD");
-doit("cp dev/win32/SDL_image.dll $WINBUILD");
-doit("cp dev/win32/pcre3.dll $WINBUILD");
-doit("cp dev/win32/libjpeg-9.dll $WINBUILD");
-doit("cp dev/win32/libpng15-15.dll $WINBUILD");
-doit("cp dev/win32/zlib1.dll $WINBUILD");
-doit("cp dev/win32/glew32.dll $WINBUILD");
-doit("cp dev/win32/freetype6.dll $WINBUILD");
+OutFile "LOGSTALGIA_INSTALLER"
 
-chdir($WINBUILD);
-doit("zip -r $APP_NAME-$VERSION.win32.zip *");
+!define MUI_WELCOMEFINISHPAGE_BITMAP   "..\..\nsis\welcome.bmp"
+!define MUI_UNWELCOMEFINISHPAGE_BITMAP "..\..\nsis\welcome.bmp"
 
-doit("cp $APP_NAME-$VERSION.win32.zip ..");
-chdir("$WINBUILD/..");
-doit("rm -rf $WINBUILD");
+!define MUI_COMPONENTSPAGE_NODESC
 
+!insertmacro MULTIUSER_PAGE_INSTALLMODE
+!insertmacro MUI_PAGE_WELCOME
+!insertmacro MUI_PAGE_COMPONENTS
+!insertmacro MUI_PAGE_DIRECTORY
+!insertmacro MUI_PAGE_INSTFILES
+!insertmacro MUI_PAGE_FINISH
+
+!insertmacro MUI_LANGUAGE "English"
+
+Function .onInit
+  !insertmacro MULTIUSER_INIT
+  ReadRegStr $R0 SHCTX "Software\Microsoft\Windows\CurrentVersion\Uninstall\Logstalgia" "UninstallString"
+  StrCmp $R0 "" done
+  MessageBox MB_OKCANCEL|MB_ICONEXCLAMATION \
+  "Logstalgia appears to already be installed. $\n$\nClick OK to remove the previous version and continue the installation." \
+  IDOK uninst
+  Abort
+ 
+ uninst:
+  ClearErrors
+  ExecWait $R0
+
+ done:
+ 
+FunctionEnd
+
+Function un.onInit
+  !insertmacro MULTIUSER_UNINIT
+FunctionEnd 
+
+Section "Logstalgia" SecLogstalgia
+  SectionIn RO
+
+  LOGSTALGIA_INSTALL_LIST
+
+  writeUninstaller $INSTDIR\uninstall.exe
+
+  WriteRegStr SHCTX "Software\Logstalgia" ${MULTIUSER_INSTALLMODE_INSTDIR_REGISTRY_VALUENAME} "$INSTDIR"
+  WriteRegStr SHCTX "Software\Logstalgia" ${MULTIUSER_INSTALLMODE_DEFAULT_REGISTRY_VALUENAME} "$MultiUser.InstallMode"
+
+  WriteRegStr   SHCTX "Software\Microsoft\Windows\CurrentVersion\Uninstall\Logstalgia" "DisplayName"          "Logstalgia"
+  WriteRegStr   SHCTX "Software\Microsoft\Windows\CurrentVersion\Uninstall\Logstalgia" "DisplayVersion"       "LOGSTALGIA_VERSION"
+  WriteRegStr   SHCTX "Software\Microsoft\Windows\CurrentVersion\Uninstall\Logstalgia" "UninstallString"      '"$INSTDIR\uninstall.exe"'
+  WriteRegStr   SHCTX "Software\Microsoft\Windows\CurrentVersion\Uninstall\Logstalgia" "QuietUninstallString" '"$INSTDIR\uninstall.exe" /S'
+  WriteRegDWORD SHCTX "Software\Microsoft\Windows\CurrentVersion\Uninstall\Logstalgia" "NoModify" 1
+  WriteRegDWORD SHCTX "Software\Microsoft\Windows\CurrentVersion\Uninstall\Logstalgia" "NoRepair" 1
+
+SectionEnd
+
+Section "Add to PATH" SecAddtoPath
+ 
+  ${If} $MultiUser.InstallMode == "AllUsers"
+    ${EnvVarUpdate} $0 "PATH" "A" "HKLM" "$INSTDIR\cmd"
+  ${ElseIf} $MultiUser.InstallMode == "CurrentUser"
+    ${EnvVarUpdate} $0 "PATH" "A" "HKCU" "$INSTDIR\cmd"
+  ${EndIf}
+
+SectionEnd
+
+Section "Uninstall"
+
+  ${If} $MultiUser.InstallMode == "AllUsers"
+    ${un.EnvVarUpdate} $0 "PATH" "R" "HKLM" "$INSTDIR\cmd"
+  ${ElseIf} $MultiUser.InstallMode == "CurrentUser"
+    ${un.EnvVarUpdate} $0 "PATH" "R" "HKCU" "$INSTDIR\cmd"
+  ${EndIf}
+
+  DeleteRegKey SHCTX "Software\Microsoft\Windows\CurrentVersion\Uninstall\Logstalgia"
+  DeleteRegKey SHCTX "Software\Logstalgia"
+
+  LOGSTALGIA_UNINSTALL_LIST
+  LOGSTALGIA_UNINSTALL_DIRS
+
+  Delete $INSTDIR\uninstall.exe
+  RMDir "$INSTDIR"
+SectionEnd
+];
+
+my @logstalgia_files = qw(
+    logstalgia.exe
+    data/example.log
+    data/ball.tga
+    data/glow.tga
+    data/fonts/FreeMonoBold.ttf
+    data/fonts/FreeSerif.ttf
+    cmd/logstalgia.cmd
+    cmd/logstalgia
+);
+
+my @logstalgia_txts = qw(
+    README
+    ChangeLog
+    data/fonts/README
+    README-SDL
+    COPYING
+    THANKS
+);
+
+my @logstalgia_dlls = qw(
+    SDL2.dll
+    SDL2_image.dll
+    pcre3.dll
+    libjpeg-9.dll
+    libpng16-16.dll
+    zlib1.dll
+    glew32.dll
+    freetype6.dll
+);
+
+my @logstalgia_dirs = qw(
+    data
+    data/fonts
+    cmd
+);
+
+my $tmp_dir = "$builds_dir/logstalgia-build.$$";
+
+doit("rm $tmp_dir") if -d $tmp_dir;
+mkdir($tmp_dir);
+
+# create directories
+foreach my $dir (@logstalgia_dirs) {
+    mkdir("$tmp_dir/$dir");
+}
+
+my @logstalgia_bundle;
+
+# copy general files
+foreach my $file (@logstalgia_files) {
+    doit("cp $file $tmp_dir/$file");
+    push @logstalgia_bundle, $file;
+}
+
+# copy dlls
+foreach my $file (@logstalgia_dlls) {
+    doit("cp $dll_dir/$file $tmp_dir/$file");
+    push @logstalgia_bundle, $file;
+}
+
+# convert text files
+foreach my $file (@logstalgia_txts) {
+    dosify("$file", "$tmp_dir/$file.txt");
+    push @logstalgia_bundle, "$file.txt";
+}
+
+my $version = logstalgia_version();
+
+my $installer_name = "logstalgia-${version}-setup.exe";
+my $archive_name   = "logstalgia-${version}.win32.zip";
+
+my $install_list = '';
+
+foreach my $dir ('', @logstalgia_dirs) {
+
+    my @dir_files = map  { my $f = $_; $f =~ s{/}{\\}g; $f; }
+                    grep { my $d = /^(.+)\// ? $1 : ''; $d eq $dir }
+                    @logstalgia_bundle;
+
+    (my $output_dir = $dir) =~ s{/}{\\}g;
+
+    $install_list .= "\n" . '  SetOutPath "$INSTDIR' . ( $dir ? "\\$output_dir" : "" ) . "\"\n\n";
+
+    foreach my $file (@dir_files) {
+        $install_list .= '  File '.$file."\n";
+    }
+}
+
+my $uninstall_list = join("\n", map { my $f = $_; $f =~ s{/}{\\}g; '  Delete $INSTDIR\\'.$f } @logstalgia_bundle);
+my $uninstall_dirs = join("\n", map { my $d = $_; $d =~ s{/}{\\}g; '  RMDir  $INSTDIR\\'.$d } reverse @logstalgia_dirs);
+
+$nsis_script =~ s/LOGSTALGIA_VERSION/$version/g;
+$nsis_script =~ s/LOGSTALGIA_INSTALLER/$installer_name/g;
+$nsis_script =~ s/LOGSTALGIA_INSTALL_LIST/$install_list/;
+$nsis_script =~ s/LOGSTALGIA_UNINSTALL_LIST/$uninstall_list/;
+$nsis_script =~ s/LOGSTALGIA_UNINSTALL_DIRS/$uninstall_dirs/;
+$nsis_script =~ s/\n/\r\n/g;
+
+chdir($tmp_dir) or die("failed to change directory to '$tmp_dir'\n");
+
+# remove existing copies of the version installer if they exist
+
+doit("rm ../$installer_name") if -e "../$installer_name";
+doit("rm ../$archive_name")   if -e "../$archive_name";
+
+my $output_file = "logstalgia.nsi";
+
+open my $NSIS_HANDLE, ">$output_file" or die("failed to open $output_file: $!");
+print $NSIS_HANDLE $nsis_script;
+close $NSIS_HANDLE;
+
+# generate installer
+
+# assert we have the long string build of NSIS
+doit("makensis -HDRINFO | grep -q NSIS_MAX_STRLEN=8192");
+
+doit("makensis $output_file");
+
+doit("rm $output_file");
+doit("mv $installer_name ..");
+
+# also create zip archive
+
+doit("zip -r $archive_name *");
+doit("mv $archive_name ..");
+
+chdir("$tmp_dir/..");
+doit("rm -rf $tmp_dir");
